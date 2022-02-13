ead03af944cb801d7fa38a8eb0933f0f5b512db3
HuuTai <huutai7738@gmail.com>
2022-02-13 19:30:49 +0700

    Show list product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,28 @@ User.create!(name: "Admin",
   phone: Faker::PhoneNumber.phone_number,
   role: 1)
 
-  10.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
-    password = "password"
-    User.create!(name: name,
-                 email: email,
-                 password: password,
-                 password_confirmation: password,
-                 phone: Faker::PhoneNumber.phone_number,
-    )
-  end
+10.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                phone: Faker::PhoneNumber.phone_number
+  )
+end
+
+5.times do
+  Category.create(name:Faker::Food.sushi)
+end
+
+30.times do
+  Product.create(name: Faker::Food.sushi,
+                 description: Faker::Food.description,
+                 status: 0,
+                 price: Faker::Commerce.price(range: 1..100.0, as_string: true),
+                 unit: "1kg",
+                 quantity: Faker::Number.between(from: 90, to: 100),
+                 category_id: Category.pluck(:id).sample)
+end

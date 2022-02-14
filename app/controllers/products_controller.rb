c0c2431431da0dea3bcd01cc85a25bb484322fb2
HuuTai <huutai7738@gmail.com>
2022-02-14 18:22:05 +0700

    Show product details

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,12 @@ class ProductsController < ApplicationController
     @pagy, @products = pagy Product.activated_products,
                             items: Settings.item_per_page
   end
+
+  def show
+    @product = Product.find_by id: params[:id]
+    return if @product
+
+    flash[:danger] = t "not_found"
+    redirect_to root_path
+  end
 end
